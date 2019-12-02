d86d470aee95af77e01e892b11f3d365537a1dec
Friedrich Delgado <delgado@dfn-cert.de>
2019-12-02 12:10:30 +0100

    reorder code
    The casual/first time user just needs to use the
    DatatablesFilterBackend, the mixins are for more advanced use-cases.

--- rest_framework_datatables/django_filters/filters.py
+++ rest_framework_datatables/django_filters/filters.py
@@ -25,6 +25,49 @@ class DatatablesFilterSet(FilterSet):
             filter_.search_regex = datatables_query['search_regex']
 
 
+class DatatablesFilterBackend(filters.DatatablesFilterBackend,
+                              DjangoFilterBackend):
+
+    filterset_base = DatatablesFilterSet
+
+    def filter_queryset(self, request, queryset, view):
+        if request.accepted_renderer.format != 'datatables':
+            return queryset
+        filtered_count_before = self.count_before(queryset, view)
+
+        filterset = self.get_filterset(request, queryset, view)
+        if filterset is None:
+            self.count_after(view, filtered_count_before)
+            return queryset
+
+        if not filterset.is_valid() and self.raise_exception:
+            raise utils.translate_validation(filterset.errors)
+        # TODO combine the global search with OR for every field
+        queryset = filterset.qs
+
+        self.count_after(view, queryset.count())
+        return queryset
+
+    def get_filterset_kwargs(self, request, queryset, view):
+        # parse query params
+        query = self.parse_query_params(request, view)
+        return {
+            'data': query['form_fields'],
+            'queryset': queryset,
+            'request': request,
+            'datatables_query': query
+        }
+
+    def parse_query_params(self, request, view):
+        query = super(DatatablesFilterBackend,
+                      self).parse_query_params(request, view)
+        form_fields = {}
+        for f in query['fields']:
+            form_fields[f['data']] = f['search_value']
+        query['form_fields'] = form_fields
+        return query
+
+
 class GlobalFilterMixin(CharFilter):
 
     def filter(self, qs, value):
@@ -75,46 +118,3 @@ class GlobalRegexFilterMixin(GlobalFilterMixin):
                 return super(GlobalRegexFilterMixin, self).filter_global(
                     qs, search_value)
         return qs
-
-
-class DatatablesFilterBackend(filters.DatatablesFilterBackend,
-                              DjangoFilterBackend):
-
-    filterset_base = DatatablesFilterSet
-
-    def filter_queryset(self, request, queryset, view):
-        if request.accepted_renderer.format != 'datatables':
-            return queryset
-        filtered_count_before = self.count_before(queryset, view)
-
-        filterset = self.get_filterset(request, queryset, view)
-        if filterset is None:
-            self.count_after(view, filtered_count_before)
-            return queryset
-
-        if not filterset.is_valid() and self.raise_exception:
-            raise utils.translate_validation(filterset.errors)
-        # TODO combine the global search with OR for every field
-        queryset = filterset.qs
-
-        self.count_after(view, queryset.count())
-        return queryset
-
-    def get_filterset_kwargs(self, request, queryset, view):
-        # parse query params
-        query = self.parse_query_params(request, view)
-        return {
-            'data': query['form_fields'],
-            'queryset': queryset,
-            'request': request,
-            'datatables_query': query
-        }
-
-    def parse_query_params(self, request, view):
-        query = super(DatatablesFilterBackend,
-                      self).parse_query_params(request, view)
-        form_fields = {}
-        for f in query['fields']:
-            form_fields[f['data']] = f['search_value']
-        query['form_fields'] = form_fields
-        return query
